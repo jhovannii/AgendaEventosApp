df16c76beee5eda135ba481cbb7f43921704b4ea
jhovannii <Uriel209)!>
2019-05-27 22:23:43 -0500

    Update proyecto
    Se sube para continuar en diferentes equipos

--- app/src/main/java/mx/ipn/escom/agendaeventosapp/SplashActivity.java
+++ app/src/main/java/mx/ipn/escom/agendaeventosapp/SplashActivity.java
@@ -1,4 +1,53 @@
 package mx.ipn.escom.agendaeventosapp;
 
-public class SplashActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import mx.ipn.escom.agendaeventosapp.ui.MainActivity;
+
+public class SplashActivity extends Activity {
+
+    private long splashDelay = 5000;
+    private Handler mTimerHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startAnimations();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                mTimerHandler.post(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, splashDelay);
+    }
+
+    private void startAnimations() {
+//        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+//        anim.reset();
+//        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+//        l.clearAnimation();
+//        l.startAnimation(anim);
+//        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+//        anim.reset();
+//        ImageView iv = (ImageView) findViewById(R.id.logo_splash);
+//        iv.clearAnimation();
+//        iv.startAnimation(anim);
+    }
 }
